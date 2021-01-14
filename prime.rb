3eae2b6abba05abad53747f36a33ec68d2d56b50
github username <github email address>
2021-01-14 16:59:45 -0500

    commit tree 4844c8ebbe91f6a758221c41a6abf9e0eb8ab8c5

--- prime.rb
+++ prime.rb
@@ -1,12 +1,7 @@
 # Add  code here!
 
 def prime?(num)
-    if num == 2
-        return true
-    elsif num > 2 && num.odd?
-        dividers = (2..num-1).to_a
-        dividers.all? {|divider| num % divider != 0}
-    else
-        return false
-    end
+        num > 1 ? (2..num-1).all? {|divider| num % divider != 0} : false
 end
+
+
