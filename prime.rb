dc43633ae8f7d8c287265dd6a1fb36075ad4db4b
github username <github email address>
2021-01-14 16:30:25 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(num)
+    if num == 2
+        return true
+    elsif num > 2 && num.odd?
+        dividers = (2..num-1).to_a
+        dividers.all? {|divider| num % divider != 0}
+    else
+        return false
+    end
+end
